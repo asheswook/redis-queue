d57e632d3b697b8f6f5c620b9f55f9c86a4a409b
Jaewook Lee <wookboy00@naver.com>
2024-03-05 22:09:29 +0900

    Feat: message structs implemented, CommonMessage and SafeMessage

---
+++ message.go
@@ -0,0 +1,57 @@
+package redis_queue
+
+import (
+	"errors"
+	"github.com/redis/go-redis/v9"
+)
+
+type Message interface {
+	Payload() string
+}
+
+func NewCommonMessage(payload string) *CommonMessage {
+	return &CommonMessage{payload: payload}
+}
+
+func NewSafeMessage(payload string, prefix string, queue *SafeQueue) *SafeMessage {
+	return &SafeMessage{payload: payload, prefix: prefix, safe: true, queue: queue}
+}
+
+// CommonMessage is a simple message
+type CommonMessage struct {
+	payload string
+}
+
+func (msg *CommonMessage) Payload() string {
+	return msg.payload
+}
+
+// SafeMessage is a message that can be acked.
+// When you receive a message from SafePop, you can ack it.
+// If you don't ack it, the message will be popped again after a while.
+type SafeMessage struct {
+	payload string
+	prefix  string
+	queue   *SafeQueue
+}
+
+func (msg *SafeMessage) Payload() string {
+	return msg.payload
+}
+
+func (msg *SafeMessage) Ack() error {
+	_, err := ack.Run(msg.queue.ctx, msg.queue.rdb, []string{msg.queue.AckName}, msg.value()).Result()
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			// Lua returns false, which means failed to ack
+			return ErrAckFailed
+		}
+		return err
+	}
+
+	return nil
+}
+
+func (msg *SafeMessage) value() string {
+	return msg.prefix + msg.payload
+}
